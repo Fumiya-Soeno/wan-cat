95de207a26a2ac2ce0705b709b276b1d1b373ef1
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2020-07-08 08:30:21 +0900

    テキストreturnでtextViewに表示

--- wan-cat/RealmSwift.swift
+++ wan-cat/RealmSwift.swift
@@ -5,4 +5,5 @@ class Human: Object {
     @objc dynamic var name = ""
     @objc dynamic var age  = 0
     @objc dynamic var sex  = ""
+//    @objc dynamic var createdAt = Date()
 }
--- wan-cat/ViewController.swift
+++ wan-cat/ViewController.swift
@@ -1,15 +1,18 @@
 import UIKit
 import RealmSwift
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     var catCount = 0
     var dogCount = 0
     @IBOutlet weak var label: UILabel!
-        @IBOutlet weak var buttonCat: UIButton!
-        @IBOutlet weak var buttonDog: UIButton!
+    @IBOutlet weak var buttonCat: UIButton!
+    @IBOutlet weak var buttonDog: UIButton!
+    @IBOutlet weak var textView: UITextView!
     
-        override func viewDidLoad() {
+    
+    override func viewDidLoad() {
             super.viewDidLoad()
+            textView.text = "あああああ"
             label.text = "どっち?"
             buttonCat.setTitle("🐱", for: .normal)
             buttonDog.setTitle("🐶", for: .normal)
@@ -19,28 +22,40 @@ class ViewController: UIViewController {
             buttonDog.titleLabel?.font = buttonSize
         }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textView.text = textField.text
+        return true
+    }
+    
     func tapped(model: Optional<String>, count: inout Int){
         guard let unwrapped = model else { return }
         count += 1
         label.text = "\(unwrapped)+\(count)"
-        
-        let realm = try! Realm()
-        let tanaka = Human()
-        tanaka.name = "田中"
-        tanaka.age  = 23
-        tanaka.sex  = "MALE"
-        try! realm.write() {
-            realm.add(tanaka)
-        }
     }
     @IBAction func tapCat(_ sender: Any) {
         tapped(model: buttonCat.currentTitle, count: &catCount)
         let realm = try! Realm()
+        let human = Human()
+        human.name = "田中"
+        human.age  = 23
+        human.sex  = "MALE"
+        try! realm.write() {
+            realm.add(human)
+        }
         for num in realm.objects(Human.self){
             print(num)
         }
     }
     @IBAction func tapDog(_ sender: Any) {
         tapped(model: buttonDog.currentTitle, count: &dogCount)
+        
+        let realm = try! Realm()
+        let results = realm.objects(Human.self).filter("name == '田中'")
+        try! realm.write {
+            realm.delete(results)
+        }
+        for num in realm.objects(Human.self){
+            print(num)
+        }
     }
 }
